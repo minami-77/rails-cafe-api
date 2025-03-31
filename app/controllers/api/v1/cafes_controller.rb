4074500775c63b1ee2413f87d9081471f0a983a6
Minami Takayama <m.inam.ina03037@gmail.com>
2025-03-31 15:30:21 +0900

    add cors

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -6,7 +6,25 @@ class Api::V1::CafesController < ApplicationController
     else
       @cafes = Cafe.all
     end
-    # give me json, not html
-    render json: @cafes
+  # in the localhost browser we see json instead of html
+  # Putting the most recently created cafes first
+  render json: @cafes.order(created_at: :desc)
+  end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      # redirect_to cafe_path(@cafe)
+      render json: @cafe, status: :created
+    else
+      # render the form page, status: :unprocessable_entity
+      render json: { error: @cafe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
   end
 end
